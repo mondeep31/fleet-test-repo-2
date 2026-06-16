499ead06904e032079d264f489086218c91ea5ce
Mondeep Chetry <mandeepchetry6969@gmail.com>
2026-06-16 15:16:56 +0530

    webhook push

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
   };
   if (env && env.length > 0) {
     returnObj.env = env;
-    returnObj.message = `ENV can be read: ${env} webhook3.`;
+    returnObj.message = `ENV can be read: ${env} webhook4.`;
   } else {
     returnObj.message = "ENV cannot be read";
   }
